4754f034da15d53bf4202814d7b86a24ff066743
Thomas Jager <thomasj@thomasjager.ca>
2024-07-28 07:31:58 -0400

    Improve Xoseram PCF

--- rtl/xoseram/xosera_xoseram.sv
+++ rtl/xoseram/xosera_xoseram.sv
@@ -19,7 +19,6 @@ module xosera_xoseram(
             input  logic        bus_bytesel,       // m68k bus byte select (RGB blue when output)
             input  logic [3:0]  bus_reg_num,        // m68k bus regnum 0
             inout  logic [7:0]  bus_data,        // m68k bus regnum 1
-            // output logic        audio_l,        // m68k bus regnum 2
             output logic        audio_r,        // m68k bus regnum 3
             output logic [3:0]  dv_r,        // audio left output
             output logic [3:0]  dv_g,        // audio left output
@@ -189,7 +188,6 @@ xosera_main xosera_main(
     .bus_data_i(bus_data_in),
     .bus_data_o(bus_data_out),
     .bus_dtack_o(bus_dtack),
-    // .audio_l_o(audio_l),
     .audio_r_o(audio_r),
     .reconfig_o(reconfig),
     .boot_select_o(boot_select),
